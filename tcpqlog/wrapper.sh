4e3e26364fb8d13cc7054bf79ee4e3c16675b130
nrybowski <nicolas.rybowski@student.uclouvain.be>
2021-06-15 09:12:10 +0200

    Build virtme container in wrapper

--- tcpqlog/wrapper.sh
+++ tcpqlog/wrapper.sh
@@ -3,4 +3,5 @@
 KERNEL_SRC="${PWD}/../../mptcp_net-next"
 MNT_POINT="/kernel_sources"
 
+docker build -t virtme -f virtme.dockerfile .
 docker run -it --privileged -v "${KERNEL_SRC}":"${MNT_POINT}" -e SRC="${MNT_POINT}" --device=/dev/kvm --device=/dev/net/tun -v /sys/fs/cgroup:/sys/fs/cgroup:rw --cap-add=NET_ADMIN --cap-add=SYS_ADMIN -e VM_IP="${VM_IP}" -e SERVER_IP="${SERVER_IP}" -e CONTAINER_NAME="${CONTAINER_NAME}" -v "${PWD}/output/${CONTAINER_NAME}":/mnt --rm --name "${CONTAINER_NAME}" virtme
